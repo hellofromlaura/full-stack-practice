a5c7f529b7c7936485cf25c24cda136a2fd0598c
Laura Wolfart <laurawolfart@gmail.com>
2017-04-24 23:16:14 -0400

    list API

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
 const app = express();
-app.use(express.static('static')); //.use() mounts the middlewear
-                                   //express.static, generates a middlewear
-                                   //function that responds to a request tries
-                                   //to match the request URL with a file.
-                                   //if the file exists, it returns the contents
-                                   //of the file
+app.use(express.static('static'));
 
-app.listen(3000, function() {
+const issues = [
+  {
+   id: 1, status: 'Open', owner: 'Ravan',
+   created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+   title: 'Error in console when clicking Add',
+ },
+ {
+   id: 2, status: 'Assigned', owner: 'Eddie',
+   created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+   title: 'Missing bottom border on panel',
+  }
+];
+
+app.get('/api/issues', (req, res) => {
+  const metadata = {total_count: issues.length};
+  res.json({_metadata: metadata, records: issues});
+});
+
+app.listen(3000, () => {
   console.log('App started on port 300');
 });

f50c412ca3bba0e89f17158f05e9b9370a0c50eb
Laura Wolfart <laurawolfart@gmail.com>
2017-07-22 22:16:07 -0400

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
 import IssueList from './IssueList.jsx';
+import IssueEdit from './IssueEdit.jsx';
 
 const contentNode = document.getElementById('contents');
-ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
+const NoMatch = () => <p>Page Not Found</p>;
+
+const RoutedApp = () => (
+  <Router history={hashHistory} >
+    <Route path="/" component={IssueList} />
+    <Route path="/issueEdit" component={IssueEdit} />
+    <Route path="*" component={NoMatch} />
+  </Router>
+);
+
+ReactDOM.render(<RoutedApp />, contentNode);
 
 if (module.hot) {
   module.hot.accept();
